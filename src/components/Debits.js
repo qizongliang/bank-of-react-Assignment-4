52b24e0c1c41024871670403e5a5d9543b302682
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-11-05 16:06:43 -0400

    Add credit done

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -67,7 +67,7 @@ const Debits = (props) => {
 
       <form onSubmit={e => {handleSubmit(e)} }>
         <input type="text" name="description" />
-        <input type="number" name="amount" />
+        <input type="number" name="amount" step=".01"/>
         <button type="submit">Add Debit</button>
       </form>
       <br/>
